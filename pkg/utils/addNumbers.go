f38030dd6a22c4ac633fc61e88e8b633485b730b
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-11-20 20:45:51 -0300

    🚧 Creating TODO list for the `addStringNumbers` algorithm

--- pkg/utils/addNumbers.go
+++ pkg/utils/addNumbers.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
-	"fmt"
+	"errors"
+	"strings"
 )
+
+var ErrInputWithDifferentNumbersCount = errors.New("input with different numbers count")
 
 // AddNumbers takse two string params containing M numbers
 // separated by spaces and returns sum of the pairs.
@@ -20,6 +23,40 @@ import (
 //
 //  >> AddNumbers("1234567.8901 2.345", "12.34 2345678901.2")
 //  "1234582.2301 2345678903.545"
-func AddNumbers(lhs, rhs string) (string, error) {
-	return "", fmt.Errorf("AddNumbers not implemented")
+func AddNumbers(lhs, rhs string) (zero string, err error) {
+	lhsElements, rhsElements := strings.Split(lhs, " "), strings.Split(rhs, " ")
+
+	if len(lhsElements) != len(rhsElements) {
+		return zero, ErrInputWithDifferentNumbersCount
+	}
+
+	result := make([]string, len(lhsElements))
+
+	for idx := 0; idx < len(lhsElements); idx++ {
+		// TODO: chekIsInBounds
+		lhsElm, rhsElm := lhsElements[idx], rhsElements[idx]
+
+		var r string
+		r, err = addStringNumbers(lhsElm, rhsElm)
+		if err != nil {
+			return zero, err
+		}
+
+		result[idx] = r
+	}
+
+	resultString := strings.Join(result, " ")
+
+	return resultString, nil
+}
+
+func addStringNumbers(lhs, rhs string) (string, error) {
+	// TODO:
+	// 1. Make sure that len(lhs) >= len(rhs)
+	// 2. Split lhs and rhs into two parts: integer and decimal
+	// 3. Iterate over len(lhs) from right to left
+	// 4. Get index values for lhs and rhs
+	// 6. If the sum is greater than 10, then set the carry to 1
+	// 7. Add the sum to the result
+	return "", nil
 }
