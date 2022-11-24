cac694fdb3b9cc686b931ff8c5cfc64d6f8de920
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-11-24 17:47:08 -0300

    ✨ Implement `Add` method for `BigInt`

--- pkg/bigint/bigint.go
+++ pkg/bigint/bigint.go
@@ -2,6 +2,7 @@ package bigint
 
 import (
 	"errors"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
@@ -9,11 +10,18 @@ import (
 	"teladoc/pkg/utils"
 )
 
-type BigInt struct {
-	magnitude []uint32
-}
-
+// ErrInvalidIntegerNumber is returned when the input string is not a valid integer number
 var ErrInvalidIntegerNumber = errors.New("invalid integer number")
+
+// BigInt represents a large integer number
+type BigInt struct {
+	// magnitude is where the number is stored in chunks
+	magnitude []uint32
+	// length represents the number of digits in the BigInt
+	length int
+	// chukSize represents the number of digits in each chunk
+	chukSize int
+}
 
 // IntegerNumberMatch is a regex that matches an integer number (without decimal places)
 // Ex: 123, 123456789012345678901234567890
@@ -21,14 +29,25 @@ var ErrInvalidIntegerNumber = errors.New("invalid integer number")
 // [101 reference](https://regex101.com/r/3hoFC3/1)
 const IntegerNumberMatch = "^[0-9]+$"
 
+// NewBigInt creates a new BigInt from a string
+// The string must be a valid integer number
+// and must not contain any decimal places
+//
+// Ex: 123, 123456789012345678901234567890
 func NewBigInt(value string) (*BigInt, error) {
 	// Validate input value
-	if match, err := regexp.MatchString(IntegerNumberMatch, value); !match || err != nil {
+	match, err := regexp.MatchString(IntegerNumberMatch, value)
+	if !match || err != nil {
 		return nil, ErrInvalidIntegerNumber
 	}
 
-	// Break the string into chunks of 9 digits
-	chunks := utils.ChunkString(value, 9)
+	// Break the string into chunks of 8 digits
+	// Breaking in chunks of 8 digits allows us to use uint32
+	// to store and perform the addition operation on the number
+	// TODO: Invsigate if we can use any other data type
+	chunkSize := 9
+	chunks := utils.ChunkString(value, chunkSize)
+
 	magnitude := make([]uint32, len(chunks))
 
 	// Convert each chunk to uint32
@@ -43,13 +62,20 @@ func NewBigInt(value string) (*BigInt, error) {
 
 	bigInt := &BigInt{
 		magnitude: magnitude,
+		length:    len(value),
+		chukSize:  chunkSize,
 	}
 
 	return bigInt, nil
 }
 
+// Length returns the number of digits in the BigInt
+func (b BigInt) Length() int {
+	return b.length
+}
+
 // String returns the string representation of the BigInt
-func (b *BigInt) String() string {
+func (b BigInt) String() string {
 	var sb strings.Builder
 
 	for _, chunk := range b.magnitude {
@@ -58,4 +84,81 @@ func (b *BigInt) String() string {
 	}
 
 	return sb.String()
+}
+
+// Add adds two BigInts and returns the result
+func (b BigInt) Add(other *BigInt) *BigInt {
+	lhs, rhs := b.magnitude, other.magnitude
+
+	// Make sure the larger magnitude is always on the left
+	if b.Length() < other.Length() {
+		lhs, rhs = rhs, lhs
+	}
+
+	// Create a new BigInt to hold the result
+	result := &BigInt{
+		magnitude: make([]uint32, len(lhs)),
+	}
+
+	// Siplify the addition for single chuck setup
+	if len(lhs) == 1 {
+		result.magnitude[0] = lhs[0] + rhs[0]
+
+		return result
+	}
+
+	var carry bool
+
+	for offset := 1; offset <= len(lhs); offset++ {
+		// Get the the chunk index
+		index := len(lhs) - offset
+
+		// Get the chunk values, rhs may be shorter than lhs
+		// so we need to check if the index is out of bounds
+		// and if so, default to `0` as the value
+		var (
+			lhsChunk = lhs[index]
+			rhsChunk uint32
+		)
+
+		// Get the chunk value from the right
+		// If the right chunk does not exist, use 0
+		if index < len(rhs) {
+			rhsChunk = rhs[index]
+		}
+
+		// Add the two chunks
+		sum := lhsChunk + rhsChunk
+
+		// Add the carry to the sum
+		if carry {
+			sum++
+		}
+
+		// Count the number of digits to determine if we need to carry
+		sumDigits := utils.CountDigits(int64(sum))
+
+		// If the sum doesn't fit we need to carry to the next chunk
+		carry = sumDigits > b.chukSize
+
+		if carry {
+			// Remove the carry from the sum
+			// sum %= 10**b.chukSize
+			exponential := math.Pow10(b.chukSize)
+			sum %= uint32(exponential)
+		}
+
+		// Store the sum in the result
+		result.magnitude[index] = sum
+	}
+
+	// If we have a carry left, we need to add a new chunk
+	if carry {
+		newMagnitude := make([]uint32, len(result.magnitude)+1)
+		newMagnitude[0] = 1
+		copy(newMagnitude[1:], result.magnitude)
+		result.magnitude = newMagnitude
+	}
+
+	return result
 }
--- pkg/bigint/bigint_test.go
+++ pkg/bigint/bigint_test.go
@@ -38,3 +38,52 @@ func TestNewBigInt(t *testing.T) {
 		})
 	}
 }
+
+func TestBigIntAdd(t *testing.T) {
+	tests := []struct {
+		lhs    string
+		rhs    string
+		result string
+	}{
+		{
+			lhs:    "999999999",
+			rhs:    "999999999",
+			result: "1999999998",
+		},
+		{
+			lhs:    "340282366920938463463374607431768211455",
+			rhs:    "340282366920938463463374607431768211455",
+			result: "680564733841876926926749214863536422910",
+		},
+		{
+			lhs:    "123",
+			rhs:    "456",
+			result: "579",
+		},
+		{
+			lhs:    "99",
+			rhs:    "9",
+			result: "108",
+		},
+		{
+			lhs:    "100",
+			rhs:    "9",
+			result: "109",
+		},
+	}
+
+	for idx, tc := range tests {
+		testname := fmt.Sprintf("test#%d", idx)
+
+		t.Run(testname, func(t *testing.T) {
+			bg1, _ := NewBigInt(tc.lhs)
+			bg2, _ := NewBigInt(tc.rhs)
+
+			bg3 := bg1.Add(bg2)
+
+			if bg3.String() != tc.result {
+				t.Errorf("got %v, want %v", bg3.String(), tc.result)
+			}
+		})
+	}
+}
